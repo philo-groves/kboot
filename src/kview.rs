a25aab4099bc28073284e10b44148044f9c26b6a
philo-groves <philogroves@gmail.com>
2025-10-23 19:59:04 -0400

    Add use_local_kview option (default: false)

--- src/kview.rs
+++ src/kview.rs
@@ -2,6 +2,10 @@ use anyhow::Result;
 
 use crate::{args, BUILD_DIRECTORY};
 
+const PORT: u16 = 3000;
+const REMOTE_TAG: &str = "philogroves/kview:0.1.2";
+const LOCAL_TAG: &str = "philogroves/kview_local:latest";
+
 // note: called from `ktest` module
 pub fn start_kview_if_needed(args: &Vec<String>) -> Result<()> {
     // check if there is a docker image for kview
@@ -10,8 +14,21 @@ pub fn start_kview_if_needed(args: &Vec<String>) -> Result<()> {
         return Ok(());
     }
 
+    // if port is in use, probably running outside of container already
+    if is_already_running() {
+        log::info!("Port {} is already in use, assuming kview is running.", PORT);
+        return Ok(());
+    }
+
     let workspace_directory = args::get_workspace_root(&args)?;
     let build_path = workspace_directory.join(BUILD_DIRECTORY);
+
+    let image_name = if !cfg!(feature = "use_local_kview") {
+        build_kview_image(&args)?;
+        LOCAL_TAG
+    } else {
+        REMOTE_TAG
+    };
 
     log::info!("Starting kview docker container in detached mode...");
     let mut docker_binding = std::process::Command::new("docker");
@@ -24,7 +41,7 @@ pub fn start_kview_if_needed(args: &Vec<String>) -> Result<()> {
         // volumes (local filesystem -> container mappings)
         .args(["-v", &format!("{}:/kview", build_path.display())])
         // run kview in container
-        .arg("philogroves/kview:0.1.1");
+        .arg(image_name);
 
     let output = command_builder.output()?;
     if !output.status.success() {
@@ -50,4 +67,33 @@ fn is_docker_container_with_name_running(name: &str) -> Result<bool> {
 
     let container_name = String::from_utf8_lossy(&output.stdout);
     Ok(container_name.lines().any(|line| line == name))
+}
+
+fn is_already_running() -> bool {
+    match reqwest::blocking::get("http://localhost:3000") {
+        Ok(_) => true,
+        Err(_) => false
+    }
+}
+
+fn build_kview_image(args: &Vec<String>) -> Result<()> {
+    let workspace_directory = args::get_workspace_root(&args)?;
+    let kview_path = workspace_directory.parent().unwrap().join("kview");
+
+    log::info!("Building kview docker image...");
+    let mut docker_build = std::process::Command::new("docker");
+    let command_builder = docker_build
+        .arg("build")                 // docker build command
+        .args(["-t", LOCAL_TAG]) // tag the image as kview:latest
+        .arg(".")                     // build context is current directory
+        .current_dir(&kview_path);    // set current directory to kview path
+
+    let output = command_builder.output()?;
+    if !output.status.success() {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        log::error!("Failed to build kview docker image: {}", stderr);
+        return Err(anyhow::anyhow!("Failed to build kview docker image"));
+    }
+
+    Ok(())
 }
